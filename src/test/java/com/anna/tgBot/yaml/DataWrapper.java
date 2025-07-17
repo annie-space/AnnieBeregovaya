60d7365dc8aad792a5a3ab1357ae70b04c658cbe
annab <beregovaanna333@gmail.com>
2025-07-17 12:18:19 +0300

    Fix: добавлено поле clients в DataWrapper, корректный конструктор Client

--- src/test/java/com/anna/tgBot/yaml/DataWrapper.java
+++ src/test/java/com/anna/tgBot/yaml/DataWrapper.java
@@ -3,16 +3,23 @@ package com.anna.tgBot.yaml;
 import java.util.List;
 
 public class DataWrapper {
+
     private List<CategoryData> categories;
+    private List<ClientData> clients;
 
     public List<CategoryData> getCategories() {
         return categories;
     }
-    public List<ClientData> getClients() { return clients; }
-    public void setClients(List<ClientData> clients) { this.clients = clients; }
 
     public void setCategories(List<CategoryData> categories) {
         this.categories = categories;
     }
-}
 
+    public List<ClientData> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<ClientData> clients) {
+        this.clients = clients;
+    }
+}
